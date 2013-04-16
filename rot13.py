d82c1d83cf8b749edc63ff6e37195d2f43cc00cb
Jose Ramon <joseoviedo.do@gmail.com>
2013-04-16 00:28:22 -0300

    Update rot13.py

--- rot13.py
+++ rot13.py
@@ -7,28 +7,27 @@
 # 
 def strSplit(cadena):
   arr = []
-	for iStr in range(0, len(cadena)):
-		arr.append(cadena[iStr])
-	return arr
+  for iStr in range(0, len(cadena)):
+      arr.append(cadena[iStr])
+  return arr
 
 #
 # @param string string
 # @return string cadena
 #
 def rot13(string):
-	abc = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-	arrAbs = strSplit(abc);
-	char = cadena = ''
-	metodo = (26 / 2)
-	for char in strSplit(string):
-		posChr = abc.find(char.upper())
-		if posChr > 0:
-			letra = (metodo + posChr) if posChr <= metodo else (posChr - metodo)
-			newChr = arrAbs[letra]
-			char = newChr.upper() if ord(newChr) > ord(char) else newChr.lower()
-		cadena += char
-	return cadena
-
-
+    abc = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    arrAbs = strSplit(abc);
+    char = cadena = ''
+    metodo = (26 / 2)
+    
+    for char in strSplit(string):
+    	posChr = abc.find(char.upper())
+    	if posChr > 0:
+    	    letra = (metodo + posChr) if posChr <= metodo else (posChr - metodo)
+	    newChr = arrAbs[letra]
+	    char = newChr.upper() if ord(newChr) > ord(char) else newChr.lower()
+	cadena += char
+     return cadena
 # ----- Fin -------- 
 print rot13(raw_input())
